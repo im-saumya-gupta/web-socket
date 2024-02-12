f940ad3330e5f245c43be346ed0f5bf25434729c
im-saumya-gupta <saumyagupt25@gmail.com>
2024-02-13 00:13:12 +0530

    socket.io implementaion complete

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const path = require("path");
-const exp = require('constants');
-
+const {Server} = require('socket.io');
+const io = new Server(server);
 
 app.use(express.static(path.resolve("./public")));
 
 app.get("/",(req,res)=>{
-    res.status(200).sendFile("/public/index.html")
-})
+    res.status(200).sendFile("/public/index.html");
+});
+
+io.on('connection',(socket)=>{
+    socket.on("user-message",(message)=>{
+        io.emit("message",message);
+    });
+//    console.log(`a new user connected with `,socket.id)
+});
 
 server.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
-})
+});
